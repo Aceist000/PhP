550b9adb98799e83ef16d61fedc136d24e3a6159
Aceist <chkaduasandro@yahoo.com>
2020-10-22 17:08:35 +0400

    week3

---
+++ week3/blog/routes/web.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+use Illuminate\Http\Request;
+// use Illuminate\Routing\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/test', function(){
+    return view('guest.form');
+});
+
+Route::post('/form', function(Request $req){
+    $data=[
+        [
+            'name'=>$req->post()['name'],
+            'lastname'=>$req->post()['lastname'],
+            'address'=>$req->post()['address'],
+            'bio'=>$req->post()['bio'],
+            'date'=>$req->post()['date'],
+        ],
+    ];
+    return view('guest.table',['data'=>$data]);
+})->name('test_form');
+
+Route::get("/index", function(){
+    return view("guest.index ");
+});
+
+
+Route::get("/single", function(){
+    return view("guest.single");
+});
